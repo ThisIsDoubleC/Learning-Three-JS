bbcb5cefabc3bd3b5b8455a270303a86ae4f2073
ThisIsDoubleC <knowledgecombinations@gmail.com>
2021-12-26 23:58:17 -0600

    Added antialiasing to renderer

--- Basic_Scene/main.js
+++ Basic_Scene/main.js
@@ -3,7 +3,7 @@
     const scene = new THREE.Scene(); //space
     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); //view into that space
 
-    const renderer = new THREE.WebGLRenderer(); //showing the view of that space
+    const renderer = new THREE.WebGLRenderer({antialias:true}); //showing the view of that space
     renderer.setSize( window.innerWidth, window.innerHeight ); //resolution (using optional updateStyle parameter) and how much of the window will we use in the window
     document.body.appendChild( renderer.domElement ); //put renderer into html code
 
